e100e1592099ab40e5742aafa153909b6309b709
Chandan-Vishwakarma1 <chandan807cv@gmail.com>
2025-05-04 15:49:56 +0530

    Validation for email and phone number

--- src/main/java/com/krutrim/service/EnrollmentService.java
+++ src/main/java/com/krutrim/service/EnrollmentService.java
@@ -5,6 +5,8 @@ import java.util.stream.Collectors;
 
 import com.krutrim.dto.EnrollmentRequestDto;
 import com.krutrim.dto.EnrollmentResponseDto;
+import com.krutrim.exception.EmailAlreadyExistsException;
+import com.krutrim.exception.PhoneAlreadyExistsException;
 import com.krutrim.mapper.EnrollmentMapper;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,18 +14,25 @@ import org.springframework.stereotype.Service;
 
 import com.krutrim.entity.Enrollment;
 import com.krutrim.repository.EnrollmentRepository;
+
 @AllArgsConstructor
 @Service
 public class EnrollmentService {
 
     @Autowired
     private EnrollmentRepository enrollmentRepository;
-    
+
     @Autowired
     private EmailService emailService;
 
     public EnrollmentResponseDto createEnrollment(EnrollmentRequestDto enrollmentRequestDto) {
 
+        if (enrollmentRepository.existsByEmail(enrollmentRequestDto.getEmail())) {
+            throw new EmailAlreadyExistsException("Email already registered");
+        }
+        if (enrollmentRepository.existsByPhone(enrollmentRequestDto.getPhone())) {
+            throw new PhoneAlreadyExistsException("Phone number already registered");
+        }
         Enrollment enrollment = EnrollmentMapper.toEntity(enrollmentRequestDto);
         Enrollment savedEnrollment = enrollmentRepository.save(enrollment);
         if (enrollment.getEmail() != null) {
